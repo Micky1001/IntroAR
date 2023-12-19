0fc00459379d9157bce40598ab3ec862705b7271
gabbykang02 <gkang9@jhu.edu>
2023-12-19 14:39:39 -0800

    initial ocmments

--- Scripts/VirtualTouch/GKButtonManager.cs
+++ Scripts/VirtualTouch/GKButtonManager.cs
@@ -5,40 +5,42 @@ using UnityEngine.UI;
 
 public class GKButtonManager : MonoBehaviour
 {
-    public TMPro.TextMeshProUGUI RedText, GreenText, BlueText, SuccessText;
-    public GameObject RedButton, GreenButton, BlueButton;
-    public Camera arcam;
+    public TMPro.TextMeshProUGUI RedText, GreenText, BlueText;		// Public UI text objects used for debugging raycast to button
+    public TMPro.TextMeshProUGUI SuccessText;	     			// Public UI text objects used for debugging sequence completion
+    public GameObject RedButton, GreenButton, BlueButton;		// Public game objects used as buttons for the sequence
+    public Camera arcam;	 	      				// AR camera
 
 
-    private Touch touch;
-    private string RedDefault, GreenDefault, BlueDefault;
-    private int press = 0;
-    private string hintText;
+    private Touch touch;						// Unity.Touch structure to store interaction screen pos
+    private string RedDefault, GreenDefault, BlueDefault;		// Corresponding default text strings for the debbugging text objects
+    private int press = 0;     		     				// Index of the current sequency entry
+    private string hintText;						// Internal boolean that stores the current progression of the sequence (and if it's correct or not). Corresponds to SuccessText debugging object
+
     // For now, correct order is blue, green, red
-
     // Start is called before the first frame update
     void Start()
     {
+	// Save the default text entries in order to reset text information when the incorrect sequence is input
         RedDefault = RedText.text;
         GreenDefault = GreenText.text;
         BlueDefault = BlueText.text;
         hintText = "- - -";
-        SuccessText.text = "puzzle start";
+        SuccessText.text = "Puzzle Start";
 
+	// Initialize completion boolean to false for game backend management
         GKBooleans.isButtonSolved = false;
-        //GKBooleans.setButtonPuzzle(false);
+
     }
 
     // Update is called once per frame
     void Update()
     {
+	// Check if a touch has been detected on the touch screen
         if (Input.touchCount > 0)
         {
-			// Get the coordinates of the center of the Button cylinder
-            //Vector3 button_pos = arcam.WorldToScreenPoint(RedButton.transform.position);
-			// Get touch objects
+	    // If so, store the touch value
             touch = Input.GetTouch(0);
-            // Declare a RaycastHit variable to store information about the collision
+            // Declare a RaycastHit variable to store 2D touch information as a ray
             Ray ray = Camera.main.ScreenPointToRay(touch.position);
             RaycastHit hit;
 
@@ -47,7 +49,8 @@ public class GKButtonManager : MonoBehaviour
             {
                 // Check for RedButton Collider
                 if (hit.collider.gameObject == RedButton) {
-                    RedText.text = RedDefault + "True";
+		    // State RedButton collision was detected if using RedText for debugging purposes
+		    RedText.text = RedDefault + "True";
                     if (press == 2) { press = 3;  SuccessText.text = "Success"; GKBooleans.isButtonSolved = true; hintText = "Success: X X X"; RedText.text = "Success: X X X"; }
                     else if (press != 3) { press = 0; SuccessText.text = "Fail"; hintText = "_ _ _"; RedText.text = hintText; }
                 }
