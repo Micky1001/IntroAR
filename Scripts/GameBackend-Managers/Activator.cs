7230227e3f1b83b998eddb34468cd9de6b7c6b2e
Micky1001 <lukeyi1001@gmail.com>
2023-12-19 23:30:17 -0500

    Update Activator.cs

--- Scripts/GameBackend-Managers/Activator.cs
+++ Scripts/GameBackend-Managers/Activator.cs
@@ -5,7 +5,8 @@ using TMPro;
 
 public class Activator : MonoBehaviour
 {
-    public GameObject puzzle0;
+    //the puzzles that to be activated throughout the gameplay
+    public GameObject puzzle0;              
     public GameObject puzzle1;
     public GameObject puzzle2;
     public TextMeshProUGUI riddle;
@@ -14,26 +15,30 @@ public class Activator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //default situation - all but expect the first puzzle/marker set to non-active
         puzzle0.SetActive(false);
         puzzle1.SetActive(false);
         puzzle2.SetActive(false);
         //puzzle3.SetActive(false);
-        riddle.text = "Starry-eyed, silent in the night,\nI'm not alive, but caught in light.\nSeek me out with a careful glance,";
+        riddle.text = "Starry-eyed, silent in the night,\nI'm not alive, but caught in light.\nSeek me out with a careful glance,"; //initial riddle 
     }
 
     // Update is called once per frame
     void Update()
     {
+        //if the first marker for the filter is used and checked, then active the second marker and change the riddle
         if (GKBooleans.isFilterChecked)
         {
             puzzle0.SetActive(true);
             riddle.text = "Among hues of warmth, I quietly dwell,\nWith a floral companion, under a spell.\nSoft as a dream, yet frozen in time,\nSeek where symbols and echoes align.";
         }
-        if(GKBooleans.isButtonSolved){
+        //if the second puzzle is solved, then active the thrid marker and change the riddle
+        if (GKBooleans.isButtonSolved){
             puzzle0.SetActive(false);
             puzzle1.SetActive(true);
             riddle.text = "Within a golden hue, dreams take flight,\nA silent sentinel guards the night.\nSeek the keeper where dreams are spun,\nIn a dance of whimsy, second to none.\n\n Tic Tac Tie";
         }
+        ////if the third puzzle is solved, then active the last marker and change the riddle
         if (GKBooleans.isTicTacSolved)
         {
             puzzle1.SetActive(false);
